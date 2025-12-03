33c24385b9e4ff1afb53ab4ae63f0702f082d9d3
CaliCodes <calista.jajalla@ftwfoundation.org>
2025-12-04 00:14:13 +0800

    Update db.py

--- etl/db.py
+++ etl/db.py
@@ -2,10 +2,12 @@ import os
 from sqlalchemy import create_engine
 
 def get_engine():
-    user = os.getenv("DB_USER", "user")
-    password = os.getenv("DB_PASSWORD", "password")
-    host = os.getenv("POSTGRES_HOST", "localhost")
-    port = os.getenv("POSTGRES_PORT", "5432")
-    db = os.getenv("POSTGRES_DB", "health_claims")
-    url = f"postgresql://{user}:{password}@{host}:{port}/{db}"
+    user = 'user'
+    password = 'password'
+    host = os.getenv('POSTGRES_HOST', 'localhost')
+    port = '5432'
+    db = 'health_claims'
+    url = f'postgresql://{user}:{password}@{host}:{port}/{db}'
+    engine = create_engine(url, pool_pre_ping=True)
     return engine
+
