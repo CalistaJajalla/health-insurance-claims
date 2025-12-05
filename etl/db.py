6e9c3122066a3d3e4bd9d9e11a13a3343f1c5e6d
CaliCodes <calista.jajalla@ftwfoundation.org>
2025-12-05 15:25:36 +0800

    Update db.py

--- etl/db.py
+++ etl/db.py
@@ -1,19 +1,19 @@
 from sqlalchemy import create_engine
-from urllib.parse import urlparse, parse_qsl, urlencode, urlunparse
+from urllib.parse import urlparse, urlunparse, parse_qs, urlencode
 
 def get_engine(secrets):
     # Cloud: use Supabase Pooler URL if available
     if "DATABASE_URL" in secrets:
         url = secrets["DATABASE_URL"]
         # Sanitize URL: remove 'pgbouncer=true' if present
-        parsed_url = urlparse(url)
-        query_params = dict(parse_qsl(parsed_url.query))
-        if 'pgbouncer' in query_params:
-            query_params.pop('pgbouncer')
-            sanitized_query = urlencode(query_params)
-            parsed_url = parsed_url._replace(query=sanitized_query)
-            url = urlunparse(parsed_url)
-        return create_engine(url, pool_pre_ping=True)
+        parsed = urlparse(url)
+        query_params = parse_qs(parsed.query)
+        query_params.pop('pgbouncer', None)  # remove pgbouncer
+        
+        new_query = urlencode(query_params, doseq=True)
+        cleaned_url = urlunparse(parsed._replace(query=new_query))
+        
+        return create_engine(cleaned_url, pool_pre_ping=True)
 
     # Local: use normal Postgres connection from individual secrets
     user = secrets["DB_USER"]
