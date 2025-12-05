faed9028463f5f8e873d7aabe8b98ed57d4b1064
CaliCodes <calista.jajalla@ftwfoundation.org>
2025-12-05 14:17:43 +0800

    Update db.py

--- etl/db.py
+++ etl/db.py
@@ -1,16 +1,16 @@
 from sqlalchemy import create_engine
 
-def get_engine():
+def get_engine(secrets):
     # Cloud: use Supabase Pooler
-    if "DATABASE_URL" in st.secrets:
-        return create_engine(st.secrets["DATABASE_URL"], pool_pre_ping=True)
+    if "DATABASE_URL" in secrets:
+        return create_engine(secrets["DATABASE_URL"], pool_pre_ping=True)
 
     # Local: use normal Postgres connection
-    user = st.secrets["DB_USER"]
-    password = st.secrets["DB_PASSWORD"]
-    host = st.secrets["POSTGRES_HOST"]
-    port = st.secrets["POSTGRES_PORT"]
-    db = st.secrets["POSTGRES_DB"]
+    user = secrets["DB_USER"]
+    password = secrets["DB_PASSWORD"]
+    host = secrets["POSTGRES_HOST"]
+    port = secrets["POSTGRES_PORT"]
+    db = secrets["POSTGRES_DB"]
 
     url = f"postgresql://{user}:{password}@{host}:{port}/{db}"
     return create_engine(url, pool_pre_ping=True)
